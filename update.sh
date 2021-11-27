0e4f34928894241d6185f77eac9922c01066d585
salleq <1240177+salleq@users.noreply.github.com>
2021-11-27 13:20:52 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#change the strings as needed
 interface=eno1
 zone=ZONE ID WHERE DOMAIN(S) TO UPDATE RESIDE
 record=DOMAIN RECORD TO UPDATE
@@ -8,6 +9,8 @@ domain=some.domain.com
 old_ipv6=$(cat $domain.ipv6) # | sed s/\ //g)
 log_file=./$domain.log
 ip6_file=./$domain.ipv6
+
+#don't change anything below unless you need to update many (sub)domains to same IP
 
 ipv6=$(ip addr show $interface | awk '/inet6 / {gsub(/\/.*/,"",$2); print $2}' | head -1)
 
@@ -36,12 +39,16 @@ echo "Updating, old ip is " $old_ipv6 "and doesn't equal new ip which is "$ipv6
 curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone/dns_records/$record" -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"id\":\"$zone\",\"type\":\"AAAA\",\"name\":\"$domain\",\"content\":\"$ipv6\",\"proxied\":false}" >> $log_file
 echo $'\n' >> $log_file
 
+# Here the following 4 lines update another domain. If zone is same as above you don't need to declare the string again.
+
+#zone=another_zone_id
 #record=record_of_another_domain
 #domain=another_domain_to_update
 #curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone/dns_records/$record" -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data "{\"id\":\"$zone\",\"type\":\"AAAA\",\"name\":\"$domain\",\"content\":\"$ipv6\",\"proxied\":false}" >> $log_file
 #echo $'\n' >> $log_file
 
-#saving current IPV6 to file
+#saving current IPV6 to file to check for next time
+
 echo $ipv6 > $ip6_file
 echo "saved current ipv6 to file"
 
